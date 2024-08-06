1ef4b37c20bf6497f0974a1c2f7805f69bd69a9d
Kishino Yuki <yktennis1124@gmail.com>
2024-08-06 01:50:04 +0000

    [finish] top,aboutページ骨組み作成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "c0e1b0b7a1e4421498026fac045fbac7.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
