fc2baa30d8e46a82ff21247279b060f64799f1a6
Kishino Yuki <yktennis1124@gmail.com>
2024-08-07 03:16:26 +0000

    [doing] 投稿モデルの作成,enumの導入,new,indexページの作成

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+    has_one_attached :image
+    enum review: {yes_and_no: 0, dissatisfied: 1, rather_dissatisfied: 2, rather_satisfied: 3, satisfied: 4}
+    
+    belongs_to :user
+end
