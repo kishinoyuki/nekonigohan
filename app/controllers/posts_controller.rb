f782fa591bc6c80d3701f753d37f00dc4afa9160
Kishino Yuki <yktennis1124@gmail.com>
2024-08-07 05:03:05 +0000

    [finish] enum実装、投稿機能のCLUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,7 +22,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
-    redirect_to post_path
+    redirect_to posts_path
   end
   
   def destroy
