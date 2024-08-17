e73ec2e94c962bd718b084db8ee3af31c6e21771
Kishino Yuki <yktennis1124@gmail.com>
2024-08-17 02:19:00 +0000

    [doing] 投稿画面のレイアウト作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,12 @@ class Item < ApplicationRecord
  end
     belongs_to :genre
     belongs_to :donation_destination
+    
+    def get_image
+     unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+     end
+     image
+    end
 end
