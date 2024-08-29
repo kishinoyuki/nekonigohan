18ae8579209d5c2d694109782c24bc2ade65ce7c
Kishino Yuki <yktennis1124@gmail.com>
2024-08-29 02:23:23 +0000

    [doing] 編集画面のバリデーション

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,18 +8,18 @@ class PostsController < ApplicationController
     @post.user_id = current_user.id
 
     @donation_destination = DonationDestination.new(name: params[:post][:donation_destination_name], location: params[:post][:donation_destination_location])
-    all_validtion = []
-    all_validtion << @donation_destination.valid?
+    all_validation = []
+    all_validation << @donation_destination.valid?
 
     @item = Item.new(name: params[:post][:item_name], genre_id: params[:post][:item_genre_id], image: params[:post][:item_image])
     @item.donation_destination = @donation_destination
-    all_validtion << @item.valid?
+    all_validation << @item.valid?
     
     # find_or_crete
     @post.item_id = @item.id
-    all_validtion << @post.valid?
+    all_validation << @post.valid?
 
-    if all_validtion == [true, true, true]
+    if all_validation == [true, true, true]
       @post.save && @item.save && @donation_destination.save
      redirect_to posts_path
     else
@@ -43,15 +43,21 @@ class PostsController < ApplicationController
   
  def update
   @post = Post.find(params[:id])
-  @item = @post.item
-  @donation_destination = @post.item.donation_destination
-  @donation_destination.update(name: params[:post][:donation_destination_name], location: params[:post][:donation_destination_location])
-  @item.update(name: params[:post][:item_name], genre_id: params[:post][:item_genre_id], image: params[:post][:item_image])
+  all_validation = []
+  all_validation << @post.valid?
   
-  if @post.update(post_params)
-     redirect_to mypage_path
+  @item = @post.item
+  all_validation << @item.valid?
+  
+  @donation_destination = @item.donation_destination
+  all_validation << @donation_destination.valid?
+  
+  if all_validation == [true, true, true]
+   @post.update(post_params) && @item.update(name: params[:post][:item_name], genre_id: params[:post][:item_genre_id]) &&@donation_destination.update(name: params[:post][:donation_destination_name], location: params[:post][:donation_destination_location])
+   redirect_to mypage_path
   else
-     render :edit
+   @post.errors.add(:base, "入力内容にエラーがあります")
+   render :edit
   end
  end
 
@@ -62,7 +68,7 @@ class PostsController < ApplicationController
   end
   
   private
-  def post_params
-    params.require(:post).permit(:title, :body, :review)
+   def post_params
+     params.require(:post).permit(:title, :body, :review)
+   end
   end
-end
