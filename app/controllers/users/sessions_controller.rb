c305427cc307790488e50f40afc6ce7ed1ba0050
Kishino Yuki <yktennis1124@gmail.com>
2024-09-02 14:22:05 +0000

    [doing]新規登録、ログインページのバリデーション設定

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,23 +9,23 @@ class Users::SessionsController < Devise::SessionsController
   def reject_user
     @user = User.find_by(email: params[:user][:email].downcase)
     if @user
+      if params[:user][:password].blank?
+        flash[:alert] = "空白の項目があります"
+      end
 
       if @user.valid_password?(params[:user][:password]) && (@user.active_for_authentication? == false)
-        flash[:notice] = "退会済みです。再度ご登録をしてご利用ください"
-  
+        flash[:alert] = "退会済みです。再度ご登録をしてご利用ください"
         redirect_to new_user_registration_path
-      else
-        flash[:notice] = "項目を入力してください"
       end
     else
-      flash[:notice] = "該当するユーザーが見つかりません"
+      flash[:alert] = "該当するユーザーが見つかりません"
     end
+    
   end
 end
   #GET /resource/sign_in
   def new
     super
-    flash
   end
 
  # POST /resource/sign_in
