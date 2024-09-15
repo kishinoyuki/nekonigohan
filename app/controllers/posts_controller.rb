df86b7abd74ad046705ec4c17901990d85fec598
Kishino Yuki <yktennis1124@gmail.com>
2024-09-15 06:57:23 +0000

    [doing] バリデーションレイアウト

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,10 +14,16 @@ class PostsController < ApplicationController
     @donation_destination = DonationDestination.new(name: params[:post][:donation_destination_name], location: params[:post][:donation_destination_location])
     all_validation = []
     all_validation << @donation_destination.valid?
+    if @donation_destination.valid? == false
+     flash[:alert] = "・寄付先を入力して下さい"
+    end
 
     @item = Item.find_or_create_by(name: params[:post][:item_name], genre_id: params[:post][:item_genre_id])
     @item.donation_destination = @donation_destination
     all_validation << @item.valid?
+    if @item.valid? == false
+     flash[:alert2] = "・商品名を入力して下さい"
+    end
     
     # find_or_crete
     @post.item_id = @item.id
