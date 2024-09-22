59dff18721c1dc8a53c0208440e9da2641f73421
Kishino Yuki <yktennis1124@gmail.com>
2024-09-22 13:12:34 +0000

    [finish] 管理者機能

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -26,6 +26,7 @@ class Admin::SessionsController < Devise::SessionsController
    end
 
    def after_sign_out_path_for(resource_or_scope)
+    flash[:success] = "ログアウトしました！"
     new_admin_session_path # ログアウト後にリダイレクトするパス
    end
 
