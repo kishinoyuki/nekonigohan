be0c7e119a30585932fe8a857d94f45ff1c7b38b
Kishino Yuki <yktennis1124@gmail.com>
2024-10-11 03:18:07 +0000

    [finish] priceカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_09_124931) do
+ActiveRecord::Schema.define(version: 2024_10_09_120345) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2024_10_09_124931) do
     t.string "name"
     t.integer "genre_id"
     t.integer "donation_destination_id"
+    t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
