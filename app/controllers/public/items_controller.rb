635b6be23d56e44178bd3ad5ecbc2be3ce8b4a4c
Kishino Yuki <yktennis1124@gmail.com>
2024-10-19 13:11:31 +0000

    [finish] item.indexのコントローラのリファクタリング

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -7,16 +7,16 @@ class Public::ItemsController < ApplicationController
   @order = params[:order]
   
   if @search.present? && @order.present?
-   @items = combined_items_search_and_order
+   @items = Item.combined_items_search_and_order(@search, @order)
   elsif @search.present? && @order.blank?
-   @items = items_by_pulldown_search
+   @items = Item.items_by_pulldown_search(@search)
   elsif @search.blank? && @order.present?
-   @items = items_by_params_order
+   @items = Item.items_by_params_order(@order)
   else
    @items = Item.all
   end 
   
-  @items = @items.page(params[:page]).per(10)
+  @items = Kaminari.paginate_array(@items).page(params[:page]).per(10)
  end
 
  def show
@@ -29,50 +29,6 @@ def redirect_unless_current_user
  unless current_user
   redirect_to root_path
  end
-end
-
-def items_by_pulldown_search
- case params[:search]
- when "~1000円"
-  Item.where(price: ..1000)
- when "1000~3000円"
-  Item.where(price: 1000..3000)
- when "3000~5000円"
-  Item.where(price: 3000..5000)
- when "5000~10000円"
-  Item.where(price: 5000..10000)
- when "10000円~"
-  Item.where(price: 10000..)
- when "食品"
-  Item.where(genre_id: 1)
- when "化粧品"
-  Item.where(genre_id: 2)
- when "キッチン用品"
-  Item.where(genre_id: 3)
- when "インテリア"
-  Item.where(genre_id: 4)
- when "日用雑貨"
-  Item.where(genre_id: 5)
- when "ペット用品"
-  Item.where(genre_id: 6)
- end
-end
-
-def items_by_params_order
- case params[:order]
- when "価格が安い商品から"
-  Item.custom_order_scope('price', 'ASC')
- when "価格が高い商品から"
-  Item.custom_order_scope('price', 'DESC')
- when "評価が低い商品から"
-  Item.average_rating('ASC')
- when "評価が高い商品から"
-  Item.average_rating('DESC')
- end
-end
-
-def combined_items_search_and_order
- items_by_pulldown_search + items_by_params_order
 end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -25,4 +25,48 @@ class Item < ApplicationRecord
       @item = Item.all
      end
    end
+   
+   def self.items_by_pulldown_search(params_search)
+    case params_search
+    when "~1000円"
+     self.where(price: ..1000)
+    when "1000~3000円"
+     self.where(price: 1000..3000)
+    when "3000~5000円"
+     self.where(price: 3000..5000)
+    when "5000~10000円"
+     self.where(price: 5000..10000)
+    when "10000円~"
+     self.where(price: 10000..)
+    when "食品"
+     self.where(genre_id: 1)
+    when "化粧品"
+     self.where(genre_id: 2)
+    when "キッチン用品"
+     self.where(genre_id: 3)
+    when "インテリア"
+     self.where(genre_id: 4)
+    when "日用雑貨"
+     self.where(genre_id: 5)
+    when "ペット用品"
+     self.where(genre_id: 6)
+    end
+   end
+   
+   def self.items_by_params_order(params_order)
+    case params_order
+    when "価格が安い商品から"
+     self.custom_order_scope('price', 'ASC')
+    when "価格が高い商品から"
+     self.custom_order_scope('price', 'DESC')
+    when "評価が低い商品から"
+     self.average_rating('ASC')
+    when "評価が高い商品から"
+     self.average_rating('DESC')
+    end
+   end
+   
+   def self.combined_items_search_and_order(params_search, params_order)
+    self.items_by_pulldown_search(params_search).items_by_params_order(params_order)
+   end
 end
