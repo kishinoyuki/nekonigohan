a52bd24cbc469c3cd557a23631e6de9baa84dce0
Kishino Yuki <yktennis1124@gmail.com>
2024-12-01 03:26:58 +0000

    [finish] relationshipモデルとuserモデルの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@ class User < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorited_posts, through: :favorites, source: :post
+  has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followeds, class_name: "Relationship", foriegn_key: "followed_id", dependent: :destroy
+  has_many :following_users, through: followers, source: followed
+  has_many :followed_users, through: followeds, source: follower
   has_one_attached :profile_image
   validates :name, presence: true
 
@@ -48,5 +52,16 @@ class User < ApplicationRecord
    def guest_user?
     email == "guest@example.com"
    end
+   
+  def follow(user_id)
+    followers.create(followed_id: user_id)
+  end
   
+  def unfollow(user_id)
+    followers.find_by(followed_id: user_id).destroy
+  end
+  
+  def following?(user)
+    following_users.include?(user)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_27_031152) do
+ActiveRecord::Schema.define(version: 2024_12_01_030349) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -106,6 +106,13 @@ ActiveRecord::Schema.define(version: 2024_10_27_031152) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "private", default: false, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "tags", force: :cascade do |t|
