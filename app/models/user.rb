ea90d09494f488cc91072869ab441f737ac9650d
Kishino Yuki <yktennis1124@gmail.com>
2024-12-13 02:17:23 +0000

    [finish] セッションタイムアウトの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :timeoutable
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
