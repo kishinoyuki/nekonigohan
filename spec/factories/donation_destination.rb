97c032ed364988829a73526584d593fdcf74c505
Kishino Yuki <yktennis1124@gmail.com>
2024-12-21 03:28:51 +0000

    [start] ログイン後のテスト項目記述

--- spec/factories/donation_destination.rb
+++ spec/factories/donation_destination.rb
@@ -1,6 +1,6 @@
-Factory.define do
+FactoryBot.define do
     factory :donation_destination do
-        name { Faker::Lrem.character(number: 10)}
+        name { Faker::Lorem.character(number: 10)}
         location {rand(0..46)}
     end
 end
