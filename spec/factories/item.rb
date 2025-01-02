8cefc2273eb4e1588ca61a195350ab68e967fe9d
Kishino Yuki <yktennis1124@gmail.com>
2025-01-02 04:08:37 +0000

    [doing] ログイン後のテスト項目チェック

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
     factory :item do
-        name { Faker::Lorem.character(number: 10)}
+        name { Faker::Lorem.characters(number: 10)}
         price {rand(300..20000)}
         genre
         donation_destination
